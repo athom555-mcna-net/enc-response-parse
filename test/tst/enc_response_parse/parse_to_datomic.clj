bbfdfcec7362884c0bd0d2a822e629adf6d8ea30
Alan Thompson <athom555@mcna.net>
2024-11-04 06:07:54 -0800

    misc

--- test/tst/enc_response_parse/parse_to_datomic.clj
+++ test/tst/enc_response_parse/parse_to_datomic.clj
@@ -17,16 +17,16 @@
 ; Defines URI for local transactor in `dev` mode. Uses `data-dir` in transactor *.properties file.
 ; Default entry `data-dir=data` => /opt/datomic/data/...
 ; Absolute path entry like `data-dir=/Users/myuser/datomic-data` => that directory.
-(def db-uri-disk "datomic:dev://localhost:4334/enc-response-test")
+(def db-uri-disk-test "datomic:dev://localhost:4334/enc-response-test")
 
 (ttj/define-fixture :each
   {:enter (fn [ctx]
-            (cond-it-> (validate boolean? (d/delete-database db-uri-disk)) ; returns true/false
+            (cond-it-> (validate boolean? (d/delete-database db-uri-disk-test)) ; returns true/false
               verbose? (println "  Deleted prior db: " it))
-            (cond-it-> (validate boolean? (d/create-database db-uri-disk))
+            (cond-it-> (validate boolean? (d/create-database db-uri-disk-test))
               verbose? (println "  Creating db:      " it)))
    :leave (fn [ctx]
-            (cond-it-> (validate boolean? (d/delete-database db-uri-disk))
+            (cond-it-> (validate boolean? (d/delete-database db-uri-disk-test))
               verbose? (println "  Deleting db:      " it)))
    })
 
@@ -38,42 +38,68 @@
    :tx-size-limit               2
    })
 
+(s/defn fn->blocked :- tsk/Fn
+  [f :- tsk/Fn]
+  (s/fn [block :- [s/Any]]
+    (mapv f block)))
+
 (verify-focus
+  (let [seq1     (range 5)
+        arr1     (partition-all 2 seq1)
+        seq2     (apply glue arr1)
+        inc-1d   (fn->blocked inc)
+        inc-2d   (fn->blocked inc-1d)
+        arr1-inc (inc-2d arr1)
+        seq1-inc (apply glue arr1-inc)
+        ]
+    (is= seq1 [0 1 2 3 4])
+    (is= arr1 [[0 1]
+               [2 3]
+               [4]])
+    (is= seq1 seq2)
+
+    (is= arr1-inc [[1 2]
+                   [3 4]
+                   [5]])
+    (is= seq1-inc [1 2 3 4 5])
+    ))
+
+(verify
   (let [
-        conn        (d/connect db-uri-disk)
+        conn        (d/connect db-uri-disk-test)
 
         resp2       @(d/transact conn enc-response-schema)
         ; >>          (pp/pprint resp2)
 
-        rec-1 {:mco-claim-number                "30000062649905"
-               :iowa-transaction-control-number "62133600780000013"
-               :iowa-processing-date            "12022021"
-               :claim-type                      "D"
-               :claim-frequency-code            "7"
-               :member-id                       "1704114C"
-               :first-date-of-service           "03112021"
-               :billing-provider-npi            "1952711780"
-               :mco-paid-date                   "11242021"
-               :total-paid-amount               "000000022968"
-               :line-number                     "00"
-               :error-code                      "A00"
-               :field                           "PAID"
-               :error-field-value               ""}
-        rec-2 {:mco-claim-number                "30000062649906"
-               :iowa-transaction-control-number "62133600780000014"
-               :iowa-processing-date            "12022021"
-               :claim-type                      "D"
-               :claim-frequency-code            "1"
-               :member-id                       "1704114C"
-               :first-date-of-service           "07012021"
-               :billing-provider-npi            "1952711780"
-               :mco-paid-date                   "11242021"
-               :total-paid-amount               "000000000000"
-               :line-number                     "00"
-               :error-code                      "A00"
-               :field                           "DENIED"
-               :error-field-value               ""}
-        sample-recs [ rec-1
+        rec-1       {:mco-claim-number                "30000062649905"
+                     :iowa-transaction-control-number "62133600780000013"
+                     :iowa-processing-date            "12022021"
+                     :claim-type                      "D"
+                     :claim-frequency-code            "7"
+                     :member-id                       "1704114C"
+                     :first-date-of-service           "03112021"
+                     :billing-provider-npi            "1952711780"
+                     :mco-paid-date                   "11242021"
+                     :total-paid-amount               "000000022968"
+                     :line-number                     "00"
+                     :error-code                      "A00"
+                     :field                           "PAID"
+                     :error-field-value               ""}
+        rec-2       {:mco-claim-number                "30000062649906"
+                     :iowa-transaction-control-number "62133600780000014"
+                     :iowa-processing-date            "12022021"
+                     :claim-type                      "D"
+                     :claim-frequency-code            "1"
+                     :member-id                       "1704114C"
+                     :first-date-of-service           "07012021"
+                     :billing-provider-npi            "1952711780"
+                     :mco-paid-date                   "11242021"
+                     :total-paid-amount               "000000000000"
+                     :line-number                     "00"
+                     :error-code                      "A00"
+                     :field                           "DENIED"
+                     :error-field-value               ""}
+        sample-recs [rec-1
                      rec-2]
         resp3       @(d/transact conn sample-recs)
         ; >>          (pp/pprint resp3)
@@ -85,8 +111,8 @@
       (is (submatch? rec-1 raw-result)))
     (let [db         (d/db conn)
           raw-result (only2 (d/q '[:find (pull ?e [*])
-                                  :where [?e :mco-claim-number "30000062649906"]]
-                             db))]
+                                   :where [?e :mco-claim-number "30000062649906"]]
+                              db))]
       (is (submatch? rec-2 raw-result)))
     ))
 
