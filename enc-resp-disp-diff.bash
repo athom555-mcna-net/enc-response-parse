ebb5cf3949443c47fd1ecbcfd5f2f75bc8c4b040
Alan Thompson <athom555@mcna.net>
2024-11-17 23:05:30 -0800

    misc

---
+++ enc-resp-disp-diff.bash
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+cat > /tmp/ctx.edn <<MYDATA
+{:db-uri               "datomic:dev://localhost:4334/enc-response"
+ :tx-size-limit        500
+
+;:missing-icn-fname    "/Users/athom555/work/missing-icns-prod-small.edn"
+ :missing-icn-fname    "/Users/athom555/work/missing-icns-prod-orig.edn"
+
+;:invoke-fn  enc-response.proc/enc-resp-disp-diff
+ :invoke-fn  enc-response.proc/enc-resp-mult-count
+}
+MYDATA
+
+java -jar /Users/athom555/work/enc-response-parse/target/enc-response-parse-1.0.0-SNAPSHOT-standalone.jar  /tmp/ctx.edn 
+
