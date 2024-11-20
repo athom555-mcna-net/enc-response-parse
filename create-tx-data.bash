9ce9df211c979dbefedddcd45e6956d64848ea68
Alan Thompson <athom555@mcna.net>
2024-11-20 06:44:40 -0800

    misc

--- create-tx-data.bash
+++ create-tx-data.bash
@@ -1,20 +1,19 @@
 #!/bin/bash 
 
-cat > /tmp/ctx.edn <<ENDTAG
+cat > /tmp/ctx.edn <<ENDER
 {
  :db-uri               "datomic:dev://localhost:4334/enc-response"
-;:tx-size-limit        500
  :tx-size-limit        3
 
  :missing-icn-fname    "/Users/athom555/work/missing-icns-prod-small.edn"
 ;:missing-icn-fname    "/Users/athom555/work/missing-icns-prod-orig.edn"
 
- :icn-maps-aug-fname      "icn-maps-aug.edn"
- :tx-data-chunked-fname   "tx-data-chunked.edn"
+ :icn-maps-aug-fname   "icn-maps-aug.edn"
+ :tx-data-fname        "tx-data.edn"
 
- :invoke-fn             enc-response.proc/create-tx-data-chunked
+ :invoke-fn             enc-response.proc/icn-maps-aug->tx-data
 }
-ENDTAG
+ENDER
 
 java -jar /Users/athom555/work/enc-response-parse/target/enc-response-parse-1.0.0-SNAPSHOT-standalone.jar  /tmp/ctx.edn 
 
