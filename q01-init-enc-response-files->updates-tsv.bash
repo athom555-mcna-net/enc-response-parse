9810d797c0d9d0a249411f98f3185cc00ce3faf0
Alan Thompson <athom555@mcna.net>
2024-12-08 22:02:15 -0800

    misc

--- q01-init-enc-response-files->updates-tsv.bash
+++ q01-init-enc-response-files->updates-tsv.bash
@@ -3,8 +3,14 @@
 cat > /tmp/ctx.edn <<ENDEND
 { :invoke-fn                    enc-response.proc/init-enc-response-files->updates-tsv
 
-  :encounter-response-root-dir  "./enc-response-files-test-small" ; full data:  "/Users/athom555/work/iowa-response"
-  :plan-icn-update-tsv-fname    "./plan-icn-update-small.tsv"
+  :encounter-response-root-dir  "/Users/athom555/work/iowa-response" ; full data
+  :plan-icn-update-tsv-fname    "./plan-icn-update-full.tsv"
+
+; :encounter-response-root-dir  "./enc-response-files-test-small" ; full data:  "/Users/athom555/work/iowa-response"
+; :plan-icn-update-tsv-fname    "./plan-icn-update-small.tsv"
+
+  :db-uri  "dummy"   ; if nil or missing, derived from other above
+
 }
 ENDEND
 
