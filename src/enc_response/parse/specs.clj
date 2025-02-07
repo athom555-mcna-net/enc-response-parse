6711b7de405e6baf69501fad59aab009256b5227
Alan Thompson <athom555@mcna.net>
2025-02-07 05:42:52 -0800

    misc

--- src/enc_response/parse/specs.clj
+++ src/enc_response/parse/specs.clj
@@ -1,10 +1,7 @@
 (ns enc-response.parse.specs
   (:use tupelo.core)
   (:require
-    [clojure.java.io :as io]
-    [flatland.ordered.map :as omap]
     [schema.core :as s]
-    [tupelo.profile :as prof]
     [tupelo.schema :as tsk]
     [tupelo.string :as str]
     ))
@@ -20,7 +17,12 @@
 ; #todo add option for :failure-type [:exception or :default-result :- tsk/KeyMap]
 
 (s/def format->pattern :- tsk/KeyMap
-  "Map from format kw to regex pattern."
+  "Map from format kw to regex pattern.
+   NOTE: all fields are strings.
+     :alpha           - string of [a-zA-Z] chars
+     :digit           - string of [0-9] chars
+     :alphanumeric    - string of [0-9a-zA-Z] chars
+     :text            - string of ASCII characters  "
   {:alpha        #"\p{Alpha}*" ; *** empty string allowed ***
    :digit        #"\p{Digit}*" ; *** empty string allowed ***
    :alphanumeric #"\p{Alnum}*" ; *** empty string allowed ***
@@ -29,12 +31,7 @@
    })
 
 (s/def iowa-encounter-response :- [tsk/KeyMap]
-  "Field specs (in order) for the Iowa Encounter Response files. Named like `ENC_RESPONSE_D_20210722_071949.TXT`.
-  NOTE: all fields are strings.
-     :alpha           - string of [a-zA-Z] chars
-     :digit           - string of [0-9] chars
-     :alphanumeric    - string of [0-9a-zA-Z] chars
-     :text            - string of ASCII characters  "
+  "Field specs (in order) for the Iowa Encounter Response files. Named like `ENC_RESPONSE_D_20210722_071949.TXT`. "
   [
    {:name :mco-claim-number :format :digit :length 30} ; #todo it is a number like "30000062649905                "
    {:name :iowa-transaction-control-number :format :digit :length 17}
@@ -53,12 +50,7 @@
    ])
 
 (s/def utah-encounter-response-hdr :- [tsk/KeyMap]
-  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`.
-  NOTE: all fields are strings.
-     :alpha           - string of [a-zA-Z] chars
-     :digit           - string of [0-9] chars
-     :alphanumeric    - string of [0-9a-zA-Z] chars
-     :text            - string of ASCII characters  "
+  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`. "
   [
    {:name :field-01 :format :text :length 4} ; HDDR
    {:name :field-02 :format :text :length 4} ; MAMM
@@ -83,18 +75,13 @@
    {:name :record-category :format :text :length 1}
    {:name :error-number :format :text :length 5}
    {:name :error-severity :format :text :length 2}
-   {:name :error-field :format :text :length 20  :length-strict? false}
+   {:name :error-field :format :text :length 20 :length-strict? false}
    {:name :tcn :format :text :length 18 :length-strict? false}
    ]
   )
 
 (s/def utah-encounter-response-rec00 :- [tsk/KeyMap]
-  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`.
-  NOTE: all fields are strings.
-     :alpha           - string of [a-zA-Z] chars
-     :digit           - string of [0-9] chars
-     :alphanumeric    - string of [0-9a-zA-Z] chars
-     :text            - string of ASCII characters  "
+  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`. "
   [{:name :submitter-id :format :text :length 4} ; #todo it is a number like "30000062649905                "
    {:name :capitated-plan-id :format :text :length 20}
    {:name :related-plan-id :format :text :length 20}
@@ -105,17 +92,12 @@
    {:name :record-category :format :text :length 1}
    {:name :error-number :format :text :length 5}
    {:name :error-severity :format :text :length 2}
-   {:name :error-field :format :text :length 20 :length-strict? false }
+   {:name :error-field :format :text :length 20 :length-strict? false}
    {:name :tcn :format :text :length 18 :length-strict? false}]
   )
 
 (s/def utah-encounter-response-rec99999 :- [tsk/KeyMap]
-  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`.
-  NOTE: all fields are strings.
-     :alpha           - string of [a-zA-Z] chars
-     :digit           - string of [0-9] chars
-     :alphanumeric    - string of [0-9a-zA-Z] chars
-     :text            - string of ASCII characters  "
+  "Field specs (in order) for the Utah Encounter Response files. Named like `4950_DOHHT007992-001_15007163_20231123.txt`. "
   [
    {:name :submitter-id :format :text :length 4}
    {:name :capitated-plan-id :format :text :length 20}
